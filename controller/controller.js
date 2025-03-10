6cb99d9e7465239fcaa026caed2535b61f25b3be
Oscar Finn <52970695+OscarFinn@users.noreply.github.com>
2025-03-10 16:36:02 +0000

    fixed issues in code as directed

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,4 @@
+const { get } = require("../app");
 const model = require("../model/model")
 
 exports.getApi = (req,res,next) => {
@@ -8,7 +9,6 @@ exports.getApi = (req,res,next) => {
 exports.getTopics = (req,res,next) => {
     model.fetchTopics()
         .then((topics) => {
-            //console.log(topics, "<-- ctrl topics")
             res.status(200).send({topics:topics})
         })
         .catch(next)
@@ -24,7 +24,6 @@ exports.getArticles = (req,res,next) => {
 
 exports.getArticleById = (req,res,next) => {
     const articleId = req.params.article_id
-    //console.log(articleId, '<--- article id')
     model.fetchArticle(articleId)
         .then((article) => {
             res.status(200).send({article:article})
@@ -34,13 +33,21 @@ exports.getArticleById = (req,res,next) => {
 
 exports.getCommentsByArticle = (req,res,next) => {
     const articleId = req.params.article_id
-    model.fetchArticle(articleId)
-        .then(() => {
-             return model.fetchCommentsByArticle(articleId)
-        })
-        .then((comments) => {
+    const checkExists = model.fetchArticle(articleId)
+    const getComments = model.fetchCommentsByArticle(articleId)
+
+    Promise.all([checkExists,getComments])
+        .then(({[1]:comments}) => {
             res.status(200).send({comments:comments})
         })
         .catch(next)
+    // model.fetchArticle(articleId)
+    //     .then(() => {
+    //          return model.fetchCommentsByArticle(articleId)
+    //     })
+    //     .then((comments) => {
+    //         res.status(200).send({comments:comments})
+    //     })
+    //     .catch(next)
     
 }
